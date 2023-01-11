d8d79829bab685bac2343201e37ce4ad10dddb68
Allard Naber <a.l.naber@rug.nl>
2023-01-11 22:00:33 +0100

    Move more api calls for testing

--- src/BsMain/Api/BsCoursesApi.php
+++ src/BsMain/Api/BsCoursesApi.php
@@ -3,11 +3,29 @@
 namespace BsMain\Api;
 
 use BsMain\Data\CourseOffering;
+use BsMain\Data\GetImportJobResponse;
+use BsMain\Data\JobToken;
 
 class BsCoursesApi extends BsResourceBaseApi {
 
 	public function getCourseOffering(int $courseId): CourseOffering {
 		$response = $this->request($this->url('/lp/1.31/courses/%d', $courseId), 'the course');
 		return new CourseOffering($response);
+	}
+
+	public function importCourseContent(int $courseId, string $filename): JobToken {
+		$options = $this->addFileToMultipartOptions('file', $filename, 'application/zip');
+		return new JobToken(
+			$this->request(
+				$this->url('/le/1.51/import/%d/imports/', $courseId),
+				'the import job', 'POST', null, $options)
+		);
+	}
+
+	public function getImportCourseContentJobStatus(int $courseId, string $jobToken): GetImportJobResponse {
+		return new GetImportJobResponse($this->request(
+			$this->url('/le/1.51/import/%d/imports/%s', $courseId, $jobToken), 'the import status')
+		);
+
 	}
 }
--- src/BsMain/Api/BsUsersApi.php
+++ src/BsMain/Api/BsUsersApi.php
@@ -2,13 +2,21 @@
 
 namespace BsMain\Api;
 
+use BsMain\Data\Locale;
 use BsMain\Data\UserData;
 
 class BsUsersApi extends BsResourceBaseApi {
 
 	public function getUser(string $username): UserData {
-		$response = $this->request($this->url('/lp/1.33/users/?username=%s', $username), 'the user');
-		return new UserData($response);
+		return new UserData(
+			$this->request($this->url('/lp/1.33/users/?username=%s', $username), 'the user')
+		);
+	}
+
+	public function getMyLocale(): Locale {
+		return new Locale(
+			$this->request($this->url('/lp/1.31/accountSettings/mySettings/locale/'), 'your preferred language')
+		);
 	}
 
 

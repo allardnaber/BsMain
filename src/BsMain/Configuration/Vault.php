269f5b1807c714092554d78522ac6c956006eeac
Allard Naber <a.l.naber@rug.nl>
2023-06-21 17:43:08 +0200

    Extend vault token validity when generating config file

--- src/BsMain/Configuration/Vault.php
+++ src/BsMain/Configuration/Vault.php
@@ -28,6 +28,18 @@ class Vault {
 		if (!$this->client->setAuthenticationStrategy(new TokenAuthenticationStrategy($token))->authenticate()) {
 			throw new \RuntimeException('Could not access Vault to retrieve required tokens.');
 		}
+		$this->renewToken();
+	}
+
+	/**
+	 * Renews lease term of Vault token. The same token will be valid for another 30 days.
+	 */
+	private function renewToken(): void {
+		try {
+			$this->client->post($this->client->buildPath('/auth/token/renew-self'));
+		} catch (ClientExceptionInterface $e) {
+			// proceed @todo log
+		}
 	}
 
 	/**

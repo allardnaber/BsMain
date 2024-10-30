9d0758f218a96975dacc4775ccf22ada83f146d1
Allard Naber <allardnaber@gmail.com>
2024-10-30 16:25:30 +0100

    Include original exception in bsappapiexception

--- src/BsMain/Exception/BsAppApiException.php
+++ src/BsMain/Exception/BsAppApiException.php
@@ -2,13 +2,15 @@
 
 namespace BsMain\Exception;
 
+use Throwable;
+
 class BsAppApiException extends BsAppRuntimeException {
 	
 	private string $dataType, $appName;
 	private int $statusCode;
 	
-	public function __construct(string $method, string $dataType, int $statusCode, string $appName = 'Brightspace') {
-		parent::__construct(sprintf('error_api_%d_%s', $statusCode, $method));
+	public function __construct(string $method, string $dataType, int $statusCode, ?Throwable $cause = null, string $appName = 'Brightspace') {
+		parent::__construct(sprintf('error_api_%d_%s', $statusCode, $method), $statusCode, $cause);
 		$this->addParam(strtolower($method));
 		$this->addParam($dataType);
 		

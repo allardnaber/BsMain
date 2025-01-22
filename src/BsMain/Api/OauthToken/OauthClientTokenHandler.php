6ecd480c859e923b22964e3d28abb3c264e05c17
Allard Naber <allardnaber@gmail.com>
2025-01-22 20:20:23 +0100

    allow use of debug access token through env var

--- src/BsMain/Api/OauthToken/OauthClientTokenHandler.php
+++ src/BsMain/Api/OauthToken/OauthClientTokenHandler.php
@@ -2,6 +2,7 @@
 
 namespace BsMain\Api\OauthToken;
 
+use BsMain\Exception\BsAppRuntimeException;
 use BsMain\Exception\SafariOauthException;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use League\OAuth2\Client\Token\AccessToken;
@@ -14,25 +15,31 @@ class OauthClientTokenHandler extends OauthTokenHandler {
 	const REDIRECT_URL = 'redirect_url';
 
 	/**
-	 * Handle retrieving an oauth2 token. This method has three paths:
+	 * Handle retrieving an oauth2 token. This method has four paths:
 	 * 1. Receive ?code=: incoming token, process and save to session, only if
 	 *    we do not have a token yet (otherwise a Refresh might cause errors
 	 *      -> processInitialTokenResponse
 	 * 2. No info supplied, try to retrieve token that was acquired earlier in
 	 *    this session and refresh if it has expired. -> refreshAccessTokenIfRequired
-	 * 3. No token available yet, start authorization process -> getInitialTokenFromuser
+	 * 3. No access token available yet, check if we have a debug token set and use that one
+	 * 4. No token available yet, start authorization process -> getInitialTokenFromUser
 	 */
 	public function retrieveAccessToken(): void {
 		if (isset($_GET['code']) && isset($_GET['state']) && !isset($_SESSION[self::TOKEN_NAME])) {
 			$this->processInitialTokenResponse();
-		} elseif (isset($_SESSION[self::TOKEN_NAME])) {
-			$tokenArr = json_decode($_SESSION[self::TOKEN_NAME], true);
+		}
+		elseif (($sessionToken = self::getTokenFromSession()) !== null) {
+			$this->setAccessToken($sessionToken);
+		}
+		elseif (($debugToken = getenv('DEBUG_BS_TOKEN')) !== false) {
+			$tokenArr = json_decode($debugToken, true);
 			$this->setAccessToken(new AccessToken($tokenArr));
-		} else {
+			$this->saveTokenToSession();
+		}
+		else {
 			$this->getInitialTokenFromUser();
 		}
 	}
-
 
 	/**
 	 * @throws IdentityProviderException
@@ -45,11 +52,10 @@ class OauthClientTokenHandler extends OauthTokenHandler {
 		$this->saveTokenToSession();
 	}
 
-
 	/**
 	 * Start oauth2 authorization sequence.
 	 */
-	private function getInitialTokenFromUser() {
+	private function getInitialTokenFromUser(): void {
 		$url = $this->getProvider()->getAuthorizationUrl();
 		$_SESSION[self::STATE_NAME] = $this->getProvider()->getState();
 		$_SESSION[self::REDIRECT_URL] = $_SERVER['REQUEST_URI'] ?? '';
@@ -73,9 +79,9 @@ class OauthClientTokenHandler extends OauthTokenHandler {
 
 	/**
 	 * Prevent CSRF attacks by checking if the state is unaltered.
-	 * @throws \Exception If we see an unexpected state.
+	 * @throws BsAppRuntimeException If we see an unexpected state.
 	 */
-	private function verifyState() {
+	private function verifyState(): void{
 		if (empty($_GET['state']) ||
 				(isset($_SESSION[self::STATE_NAME]) &&
 				$_GET['state'] !== $_SESSION[self::STATE_NAME] )
@@ -83,11 +89,11 @@ class OauthClientTokenHandler extends OauthTokenHandler {
 			if (isset($_SESSION[self::STATE_NAME])) {
 				unset($_SESSION[self::STATE_NAME]);
 			}
-			throw new \Exception('Invalid state while retrieving access token.');
+			throw new BsAppRuntimeException('Invalid state while retrieving access token.');
 		}
 	}
 
-	private function processInitialTokenResponse() {
+	private function processInitialTokenResponse(): void {
 		$this->verifyState();
 		$this->setAccessToken($this->getProvider()->getAccessToken(
 				'authorization_code', ['code' => $_GET['code']]
@@ -99,7 +105,7 @@ class OauthClientTokenHandler extends OauthTokenHandler {
 		}
 	}
 
-	private function saveTokenToSession() {
+	private function saveTokenToSession(): void {
 		$_SESSION[self::TOKEN_NAME] = json_encode($this->getCurrentAccessToken()->jsonSerialize());
 	}
 
